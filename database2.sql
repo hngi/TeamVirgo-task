4f8d49d65855d7690af4df8c1ff238c7b645c6ba
Hafizah Muhyideen <40233874+justfizah@users.noreply.github.com>
2019-09-18 22:35:00 +0000

    Delete database2.sql

--- database2.sql
+++
@@ -1,1 +0,0 @@
-CREATE TABLE `virgohng2`.`virgosignup` ( `id` INT NOT NULL AUTO_INCREMENT , `username` VARCHAR(100) NOT NULL , `email` VARCHAR(100) NOT NULL , `password` VARCHAR(100) NOT NULL , PRIMARY KEY (`id`)) ENGINE = InnoDB;
